86238ffb17b0aad66f30e4a1db7c32ca1deb2bf7
Nicola Galeotti <niccio2802@outlook.it>
2020-10-31 14:01:16 +0100

    aggiunta console app di prova

---
+++ Prova/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Registro;
+
+namespace Prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scuola scuola = new Scuola("Rosselli_Rasetti");
+            Classe classe = scuola.CreateClasse(4, "AI");
+            Studente studente1 = new Studente("Nicola", "Galeotti");
+            Studente studente2 = new Studente("Narcis", "Buzatu");
+            classe.AddStudente(studente1);
+            classe.AddStudente(studente2);
+            Materia materia1 = new Materia("Matematica");
+            Valutazione voto1 = new Valutazione(8, studente1, materia1);
+            Valutazione voto2 = new Valutazione(7, studente2, materia1);
+            Valutazione votomax = scuola.TrovaVotoMassimo();
+            Console.WriteLine($"Il voto massimo della scuola {scuola.GetNome()} è {votomax.GetVotoDouble()}.");
+        }
+    }
+}
